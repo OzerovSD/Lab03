46d61c4c60147016d471c2a73a106c769eaaabff
Sergey <sereja@mail.ru>
2022-04-18 18:43:17 +0300

    code: черная гистограмма

--- main.cpp
+++ main.cpp
@@ -56,46 +56,52 @@ vector<size_t>make_histogram(const auto numbers, size_t bin_count, double max, d
     return bins;
 }
 
-void show_histogram_text(const auto bins)
-{
-    const size_t SCREEN_WIDTH=80;//Максимальная допустимая ширина дистограмм
-    const size_t MAX_ASTERISK=SCREEN_WIDTH-3-1;//Максимальное допустимое кол-во звездочек
-    size_t max_bin=bins[0];//Максимальная высота столбца;
-        for(size_t bin: bins)
-        {
-            if(max_bin<bin)
-            {
-                max_bin=bin;
-            }
-        }
-
-    for(size_t bin:bins)
-    {
-        size_t height=bin;//переменная, которую меняем для звезд
-        //Проверить, нужно ли масштабировать данные, и если нужно, то пересчитать height
-        if(max_bin>MAX_ASTERISK)
-        {
-            height=MAX_ASTERISK*(static_cast<double>(bin)/max_bin);//коэффицент
-        }
-
-        if(bin<100)
-        {
-           cout<<" ";
-        }
-         if(bin<10)
-        {
-           cout<<" ";
-        }
-
-        cout<<bin<<"|";
-        for(size_t i=0; i<height; i++)
-        {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
 }
 
+void svg_end()
+{
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='"<<baseline<<"'>"<<text<<"</text>";
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke="black", string fill="black")
+{
+    cout << "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+}
+
+void show_histogram_svg(const vector<size_t>& bins)
+{
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    const auto NAMECOLOR="darkorange";
+    const auto COLOR="#ff8c00";
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+    top += BIN_HEIGHT;
+}
+    svg_end();
+}
 
 int main()
 {
@@ -116,7 +122,9 @@ int main()
     const auto bins= make_histogram(numbers, bin_count, max, min, number_count);
 
     //Вывод гистограммы
-    show_histogram_text(bins);
+
+    show_histogram_svg(bins);
+
 
 return 0;
 }
