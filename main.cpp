9a08089807487fc40005644a39a29e5686dcaef8
serg <ser@mail.ru>
2022-06-07 15:25:35 +0300

    code: пункт 3.1.2

--- main.cpp
+++ main.cpp
@@ -15,19 +15,19 @@ vector<double>input_numbers(istream& in, size_t count)
 }
 
 
-Input read_input(istream& in) {
+Input read_input(istream& in, bool prompt)
+{
     Input data;
-
+    if (prompt)
     cerr << "Enter number count: ";
     size_t number_count;
     in >> number_count;
-
+    if (prompt)
     cerr << "Enter numbers: ";
     data.numbers = input_numbers(in, number_count);
-
-    cerr<<"Enter bin_count:";//K, кол-во столбцов(корзин)
-    cin>>data.bin_count;
-
+    if (prompt)
+    cerr << "enter bin count: ";
+    in >> data.bin_count;
     return data;
 }
 
@@ -51,10 +51,8 @@ vector<size_t>make_histogram(struct Input name)
 
 int main()
 {
-   Input results;
-   results=read_input(cin);
-    //Вывод гистограммы
-    auto bins = make_histogram(results);
+    const auto input = read_input(cin, true);
+    const auto bins = make_histogram(input);
     show_histogram_svg(bins);
 
 return 0;
