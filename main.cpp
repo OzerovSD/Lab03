1b9161914340ba0195555bb273b06d84c74144da
serg <ser@mail.ru>
2022-06-10 15:15:53 +0300

    code: загрузка файла 3.2.6

--- main.cpp
+++ main.cpp
@@ -80,6 +80,17 @@ int main(int argc, char* argv[])
         {
         input = read_input(cin, true);
         }
+    {
+        CURL *curl = curl_easy_init();
+        if(curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
+        return 0;
+    }
     curl_global_init(CURL_GLOBAL_ALL);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
